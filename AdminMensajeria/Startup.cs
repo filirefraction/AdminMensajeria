e5ea2bfc0837d3a6173de5b78092c1979fad6013
Fili <filiberto.sanchez@isbi.com.mx>
2019-08-07 21:01:32 -0500

    Se cargó el modelo de datos

---
+++ AdminMensajeria/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdminMensajeria.Startup))]
+namespace AdminMensajeria
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
